3b109d46bb82e77eddd6c260a5057d77f85007bc
mahmoudsamae <mahmoudsamaedbm@gmail.com>
2025-06-22 18:17:31 +0200

    fix frontend

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "sayzy-real-estate-booking-platform.onrender.com"],
   },
 };
 
